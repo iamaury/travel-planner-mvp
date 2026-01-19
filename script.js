091390d3f05a4ff4c9e3be90f87fbd734037e201
Amaury HUTIN <toolsah@pm.me>
2026-01-19 15:27:43 +0100

    Update script.js
    Affichage ou non de la fenetre d'errorMessage

--- script.js
+++ script.js
@@ -3,26 +3,31 @@ const joinPlanningBtnAction = document.getElementById("joinPlanningBtn");
 const planningCode = document.getElementById("joinPlanningCode");
 const accueilVue = document.getElementById("accueil");
 const planningVue = document.getElementById("planning")
+const errorMessage = document.getElementById("errorMessage")
 
 planningVue.style.display = "none";
+errorMessage.style.display = "none";
 
 planningBtnAction.addEventListener("click",function() {
   accueilVue.style.display = "none";
   planningVue.style.display = "block";
 });
+
 planningCode.addEventListener("input",function(){
+  errorMessage.style.display = "none";
   planningCode.value = planningCode.value.toUpperCase();
   if (planningCode.value.length > 6){
     planningCode.value = planningCode.value.slice (0,6);
   }
 });  
+
 joinPlanningBtnAction.addEventListener("click",function() {
   const code = planningCode.value;
   const regex = /^[A-Z0-9]{6}$/; 
   if  (regex.test(code)){
-  console.log("code ok");
+    console.log ("code ok");
   } else {
-  planningCode.value = "error";
+  errorMessage.style.display = "block";
   }
 });
 
