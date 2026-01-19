996fdbec38563a135551f7797d6de174420aff79
Amaury HUTIN <toolsah@pm.me>
2026-01-19 14:18:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const planningBtnAction = document.getElementById("createPlanningBtn"); 
+const joinPlanningBtnAction = document.getElementById("joinPlanningBtn");
+const planningBtnCode = document.getElementById("joinPlanningCode");
 const accueilVue = document.getElementById("accueil");
 const planningVue = document.getElementById("planning")
 
@@ -7,4 +9,7 @@ planningVue.style.display = "none";
 planningBtnAction.addEventListener("click",function() {
   accueilVue.style.display = "none";
   planningVue.style.display = "block";
+});
+joinPlanningBtnAction.addEventListener("click",function() {
+  console.log(planningBtnCode.value);
 });
